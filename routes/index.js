5c6119a33fa2df0126d54f4b2e7a0410719e3555
gfellone <gfellone@indracompany.com>
2019-09-16 14:44:19 -0300

    Adicionando arquivos base

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {page:'Home', menuId:'home'});
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {page:'About', menuId:'about'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {page:'Contact', menuId:'contact'});
 });
 
 module.exports = router;
